e30468659c68d3f45b726f0b23992e378a35f99e
Alexander Wolden <awolden@gmail.com>
2018-05-17 18:12:11 -0700

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -5,11 +5,13 @@ const args = require('args')
 args
   .option(['j', 'coverage-json'], 'Relative path to istanbul coverage JSON', 'coverage/coverage-final.json')
   .option(['c', 'coverage-html'], 'Relative path to coverage html root (for artifact links)', 'coverage/lcov-report')
+  .option(['g', 'github-url'], 'Github Url', 'api.github.com')
   .option(['b', 'branch'], 'Base branch to use if not PR', 'master')
 
 const {
   coverageJson,
   coverageHtml,
+  githubUrl,
   branch
 } = args.parse(process.argv)
 
@@ -20,6 +22,7 @@ try {
     root: process.cwd(),
     coverageJsonFilename: coverageJson,
     coverageHtmlRoot: coverageHtml,
+    githubUrl,
     defaultBaseBranch: branch
   }
   const url = postComment(params)
